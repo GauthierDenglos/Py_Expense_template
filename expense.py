fe3de79f74bd97ae98b2c40e021e47dc2fb2308f
GauthierDenglos <gauthier.denglos@epita.fr>
2021-11-03 21:18:01 +0100

    [ADD][features] add 7th features

--- expense.py
+++ expense.py
@@ -1,4 +1,37 @@
 from PyInquirer import prompt
+import csv
+
+def choose_users():
+    users = []
+    with open('users.csv', newline='') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            users.append(row[0])
+    return users
+
+def choose_spenders(spender):
+    users = choose_users()
+    spenders = []
+    for user in users :
+        dic = {}
+        dic["name"] = user
+        if user == spender :
+            dic["checked"] = True
+        else : 
+             dic["checked"] = False
+        spenders.append(dic)
+    return spenders
+
+def get_spencer(spender):
+    expense_questions = [
+        {
+            "type":"checkbox",
+            "name":"involves",
+            "message":"Add new spenders : ",
+            "choices": choose_spenders(spender)
+        },
+    ]
+    return expense_questions
 
 expense_questions = [
     {
@@ -12,19 +45,22 @@ expense_questions = [
         "message":"New Expense - Label: ",
     },
     {
-        "type":"input",
+        "type":"list",
         "name":"spender",
-        "message":"New Expense - Spender: ",
+        "message":"Choose a spenser :",
+        "choices": choose_users()
     },
-
 ]
-
-
 
 def new_expense(*args):
     infos = prompt(expense_questions)
-    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
-    print("Expense Added !")
+    infos_involves = prompt(get_spencer(infos.get("spender")))
+
+    expense = [infos.get('amount'), infos.get('label'), infos.get('spender'), infos_involves.get('involves')]
+    with open('expense_report.csv', 'a', encoding='UTF8') as file:
+        writer = csv.writer(file)
+        writer.writerow(expense)
+        file.close()
     return True
 
 
--- user.py
+++ user.py
@@ -1,8 +1,22 @@
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"name",
+        "message":"New User - Name: ",
+    }
 ]
 
 def add_user():
-    # This function should create a new user, asking for its name
-    return
+    infos = prompt(user_questions)
+    # print(infos)
+    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    # print("Expense Added !")
+    user = [infos.get('name')]
+    with open('users.csv', 'a', encoding='UTF8') as file:
+        writer = csv.writer(file)
+        writer.writerow(user)
+        file.close()
+    return True
